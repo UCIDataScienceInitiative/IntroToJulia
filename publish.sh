31c11d3842d3fffc2bd453df691ba99709f56896
ChrisRackauckas <me@chrisrackauckas.com>
2016-09-18 15:49:26 -0700

    Update Slides

--- publish.sh
+++ publish.sh
@@ -1,7 +1,12 @@
 !/bin/bash
 
-jupyter-nbconvert --to slides ArraysAndMatrices.ipynb --reveal-prefix=reveal.js
-mv ArraysAndMatrices.slides.html  ArraysAndMatrices.html
+# Generate the Slides and Pages
+jupyter-nbconvert --to slides Notebooks/ArraysAndMatrices.ipynb --reveal-prefix=reveal.js
+mv ArraysAndMatrices.slides.html  Slides/ArraysAndMatrices.html
+#jupyter-nbconvert --to slides ArraysAndMatrices.ipynb --reveal-prefix=reveal.js
+#mv ArraysAndMatrices.slides.html  ArraysAndMatrices.html
+
+# Push the updates to gh-pages
 mkdir -p /tmp/workspace
 cp -r * /tmp/workspace/
 git add -A .
@@ -9,7 +14,7 @@ git commit -m "Update Slides"
 git checkout -B gh-pages
 cp -r /tmp/workspace/* .
 git add -A .
-git commit -m "new version"
+git commit -m "Update Slides"
 git push origin master gh-pages
 git checkout master
 rm -rf /tmp/workspace
