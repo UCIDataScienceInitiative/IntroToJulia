948ca6490a9def495a0939925347f0c18ea5fe89
Christopher Rackauckas <Me@ChrisRackauckas.com>
2016-10-22 20:51:41 -0700

    Easter egg false

--- src/IntroToJulia.jl
+++ src/IntroToJulia.jl
@@ -1,3 +1,17 @@
 module IntroToJulia
 
+function find_easter()
+
+println("""
+Welcome to the IntroToJulia Easter Egg hunt!
+
+If you found this, the you have not successfully checked out
+the Easter branch of the repository. Please go through the
+materials again to find out how to change the branch of
+of a repository, or ask for help!
+""")
+end
+
+export find_easter
+
 end
