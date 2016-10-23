ec911fd584c8c0640c0580d047fc7c1744deb139
Christopher Rackauckas <Me@ChrisRackauckas.com>
2016-10-22 20:50:51 -0700

    find_easter

--- src/IntroToJulia.jl
+++ src/IntroToJulia.jl
@@ -1,3 +1,17 @@
 module IntroToJulia
 
+function find_easter()
+
+println("""
+Welcome to the IntroToJulia Easter Egg hunt!
+
+If you found this, the you have successfully checked out
+the Easter branch of the repository. However, there's
+still more to do! Use Github to go to the dev branch
+and look at the source code in the assets folder.
+There you will find something out of place!
+""")
+end
+
+export find_easter
 end
