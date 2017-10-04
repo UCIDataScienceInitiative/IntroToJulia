c9ad0e3179c56abfe47fa8b0b7601bf47ca6dfe1
Miguel Raz Guzmán Macedo <miguelraz@ciencias.unam.mx>
2017-10-04 13:39:45 -0500

    typo fix

--- src/IntroToJulia.jl
+++ src/IntroToJulia.jl
@@ -5,7 +5,7 @@ function find_easter()
 println("""
 Welcome to the IntroToJulia Easter Egg hunt!
 
-If you found this, the you have not successfully checked out
+If you found this, then you have not successfully checked out
 the Easter branch of the repository. Please go through the
 materials again to find out how to change the branch of
 of a repository, or ask for help!
